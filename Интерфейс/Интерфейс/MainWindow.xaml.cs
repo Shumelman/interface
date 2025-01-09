6e5baf440ba107fbe36a2961d309e04e9e2e55e0
Shumelman <146671458+Shumelman@users.noreply.github.com>
2025-01-09 22:10:07 +0300

    interface

---
+++ Интерфейс/Интерфейс/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace Интерфейс
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown (object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+    
+    }
+}
---
+++ Интерфейс/Интерфейс/UserControls/MenuBatton.xaml.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+using MahApps.Metro.IconPacks;
+
+
+namespace Интерфейс.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuBatton.xaml
+    /// </summary>
+    public partial class MenuBatton : UserControl
+    {
+        public MenuBatton()
+        {
+            InitializeComponent();
+        }
+
+        public PackIconMaterialKind Icon
+        {
+            get { return (PackIconMaterialKind) GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(PackIconMaterialKind), typeof(MenuBatton));
+
+
+
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register("IsActive", typeof(bool), typeof(MenuBatton));
+    }
+
+
+
+
+}
